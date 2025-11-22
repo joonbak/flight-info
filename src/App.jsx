e7073223bc6d4d896b3871aafeb92e3f3eb71eda
Joon Bak <jbdev98@gmail.com>
2025-11-22 17:03:15 +1100

    Add Card Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import Globe from "react-globe.gl";
 
+import {
+  Card,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+  CardAction,
+  CardContent,
+  CardFooter,
+} from "@/components/ui/card.jsx";
+
 import globeJson from "./assets/countries_110m.json";
 
 function App() {
@@ -12,9 +22,24 @@ function App() {
     },
   ];
   return (
-    <div>
-      <main>
-        <div>
+    <div className="fixed">
+      <div className="relative">
+        <div className="absolute z-10 mt-4 ml-4">
+          <Card className="w-96 h-[calc(100vh-2rem)]">
+            <CardHeader>
+              <CardTitle>Card Title</CardTitle>
+              <CardDescription>Card Description</CardDescription>
+              <CardAction>Card Action</CardAction>
+            </CardHeader>
+            <CardContent>
+              <p>Card Content</p>
+            </CardContent>
+            <CardFooter>
+              <p>Card Footer</p>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className="absolute z-0">
           <Globe
             globeOffset={[300, 0]}
             backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
@@ -26,7 +51,7 @@ function App() {
             pointColor="color"
           />
         </div>
-      </main>
+      </div>
     </div>
   );
 }
